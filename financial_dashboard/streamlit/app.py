953cffd725bce35f7d7e8a95f0026627d7114125
Harsha <109337973+harshapeteti@users.noreply.github.com>
2024-11-27 15:14:51 +0530

    Update app.py

--- financial_dashboard/streamlit/app.py
+++ financial_dashboard/streamlit/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-from scoring import finance_score  # Scoring function
+from scoring import finance_score  #scoring function
 
 @st.cache_data
 def load_data():
